60d3e61274a9bc58c2a1a172ddf5bd2cb15a6445
Giovanna Emanuely <giovannaEmma16@gmail.com>
2025-03-13 23:02:30 -0300

    Add files via upload

---
+++ 1Bimestre/PrjCalculadoraSimples/Aplicacao.java
@@ -0,0 +1,11 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Calculadora calcu = new Calculadora(); //instanciar o objeto Calculadora
+		
+		calcu.operacao();
+	}
+
+}
